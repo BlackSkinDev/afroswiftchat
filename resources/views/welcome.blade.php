a4804dfa6d22e9adb6d2c470b0245e3513f20439
azeezafeez <chillingloccin1@gmail.com>
2021-03-17 18:17:43 +0100

    changing site logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,11 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>SwiftChat</h1>
+                    <center>
+                        <div>
+                            <img src="/img/swiftchatlogo.png" height="80" width="200" class="pr-3">
+                        </div>
+                    </center>
 
 
                 </div>
